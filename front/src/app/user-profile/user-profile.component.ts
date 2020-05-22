c02e6fd3fa0d4e8c9e0ca3c54ec16a135e0b2867
Marvin Tellis <joelmarvin.t@somaiya.edu>
2020-05-22 21:17:53 +0530

    Added Scroll-to-top button and improved CSS of app

--- front/src/app/user-profile/user-profile.component.ts
+++ front/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { UserService } from '../shared/user.service';
 import { Router } from '@angular/router';
 
@@ -33,5 +33,32 @@ export class UserProfileComponent implements OnInit {
     this.router.navigateByUrl('/login');
   }
 
-  
+  isShow: boolean;
+  topPosToStartShowing = 100;
+
+  @HostListener('window:scroll')
+  checkScroll() {
+      
+    // window의 scroll top
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+    
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
 }
