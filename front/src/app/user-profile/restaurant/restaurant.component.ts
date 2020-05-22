1d6666ded89746788a12c22cc9bcb714a2eb1bab
dhruvin18 <dhruvin30@somaiya.edu>
2020-05-22 23:36:37 +0530

    space

--- front/src/app/user-profile/restaurant/restaurant.component.ts
+++ front/src/app/user-profile/restaurant/restaurant.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class RestaurantComponent implements OnInit {
 
   restaurants: Restaurant[];
-  
+
   constructor(private restaurantService: RestaurantService) { }
 
   ngOnInit(): void {
